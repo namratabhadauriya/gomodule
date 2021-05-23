6cbec999bd1e06ed62503e17b792d77249783da8
namrata.bhadauriya <namrata.nitw@gmail.com>
2021-05-23 10:55:23 +0530

    minor changes in log

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,12 @@
 package calc
 
+import "fmt"
+
 func Add(numbers ...int) int {
 	sum := 0
 	for _, value := range numbers {
 		sum = sum + value
 	}
+	fmt.Println("sum from package is", sum)
 	return sum
 }
